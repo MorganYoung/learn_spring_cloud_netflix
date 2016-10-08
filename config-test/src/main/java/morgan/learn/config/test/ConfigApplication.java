9479c0bc5d880b01c51b97550f22b6cf82d51f93
morgan <morganyang@sohu-inc.com>
2016-10-08 10:03:28 +0800

    config test

---
+++ config-test/src/main/java/morgan/learn/config/test/ConfigApplication.java
@@ -0,0 +1,18 @@
+package morgan.learn.config.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created by morgan on 16/10/4.
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
