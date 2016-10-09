7f544c7742b4b2b8c6bdc0b05a7ac842ff113b83
morgan <morganyang@sohu-inc.com>
2016-10-09 10:29:16 +0800

    add eureka test

--- eureka-test-client/src/main/java/morgan/learn/eureka/Controller.java
+++ eureka-test-client/src/main/java/morgan/learn/eureka/Controller.java
@@ -1,6 +1,7 @@
 package morgan.learn.eureka;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by morgan on 16/10/8.
  */
 @RestController
+@RefreshScope
 public class Controller {
 
     @Value("${words}")
